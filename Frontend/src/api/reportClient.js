c77a95b3172d909af4605d639fa13079287880e2
Michael Bennett <110262859+emotheatrix@users.noreply.github.com>
2023-04-10 14:02:58 -0400

    Fixed generate report methodology

---
+++ Frontend/src/api/reportClient.js
@@ -0,0 +1,46 @@
+import BaseClass from "../util/baseClass";
+import axios from 'axios'
+
+export default class ReportClient extends BaseClass {
+    constructor(props = {}) {
+        super();
+        const methodsToBind = ['clientLoaded', 'generateReport' , this.errorCallback];
+        this.bindClassMethods(methodsToBind, this);
+        this.props = props;
+        this.clientLoaded(axios);
+    }
+
+
+    clientLoaded(client) {
+        this.client = client;
+        if (this.props.hasOwnProperty("onReady")) {
+            this.props.onReady();
+        }
+    }
+
+    async generateReport(errorCallback) {
+        try {
+            const response = await this.client.get('report');
+            return response.data;
+        } catch (error) {
+            this.handleError("generateReport", error, errorCallback)
+        }
+    }
+
+    handleError(method, error, errorCallback) {
+        console.error(method + "failed - " + error);
+        if (error.response.data.message !== undefined) {
+            console.error(error.response.data.message);
+        }
+
+        if (errorCallback) {
+            errorCallback(method + "failed - " + error);
+        }
+    }
+
+    errorCallback = (error) => {
+        console.error(error);
+
+    }
+}
+
